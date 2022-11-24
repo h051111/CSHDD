1c124d14cdc9cb8a98f82863d6b4c78f129a1707
Rózsa Kristóf <h051111@stud.u-szeged.hu>
2022-11-24 16:41:38 +0100

    Init

---
+++ CSHDD/JavaScriptListener.cs
@@ -0,0 +1,59 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+
+namespace CSHDD;
+
+public class JavaScriptListener : JavaScriptParserBaseListener
+{
+    private ICharStream inputStream;
+    private Dictionary<int, List<ParserRuleContext>> levelNodes = new Dictionary<int, List<ParserRuleContext>>();
+    public void setInputStream(ICharStream inputStream)
+    {
+        this.inputStream = inputStream;
+    }
+    
+    public override void EnterAssignmentExpression(JavaScriptParser.AssignmentExpressionContext context)
+    {
+        //Console.WriteLine("------------------------------------");
+        //Console.WriteLine(context.Start.Text);
+        //Console.WriteLine("------------------------------------");
+    }
+    
+    public Dictionary<int, List<ParserRuleContext>> getLevels(IParseTree tree)
+    {
+        ParseTreeWalker.Default.Walk(this, tree);
+        return levelNodes;
+    }
+
+    private int getHeight(ParserRuleContext context)
+    {
+        int i = 0;
+        RuleContext parent = context.Parent;
+        
+        while (parent != null)
+        {
+            parent = parent.Parent;
+            i++;
+        }
+
+        return i;
+    }
+
+    public override void EnterEveryRule(ParserRuleContext context)
+    {
+        int level = getHeight(context);
+        
+        if (levelNodes.ContainsKey(level))
+        {
+            levelNodes[level].Add(context);
+        }
+        else
+        {
+            levelNodes.Add(level, new List<ParserRuleContext>());
+            levelNodes[level].Add(context);
+        }
+        
+        base.EnterEveryRule(context);
+    }
+}
---
+++ CSHDD/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+using CSHDD;
+using HDDMin;
+using static HDDMin.Utilities;
+
+String inputPath = "C:/Programming/picireny/me/tester2.js";
+String pathToTester = "C:/Programming/picireny/me/tester4.py";
+
+String inputText = File.ReadAllText(inputPath);
+ICharStream stream = CharStreams.fromString(inputText);
+
+ITokenSource lexer = new JavaScriptLexer(stream);
+ITokenStream tokens = new CommonTokenStream(lexer);
+JavaScriptParser parser = new JavaScriptParser(tokens) {BuildParseTree = true};
+
+IParseTree tree = parser.program();
+JavaScriptListener listener = new JavaScriptListener();
+
+listener.setInputStream(stream);
+
+HDD hdd = new HDD();
+
+HDDTreeNode root = hdd.Reduce(tree, pathToTester, Path.GetFileName(inputPath), stream);
+String newSource = TreeToSource(root);
+
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("REDUCED TO:");
+Console.WriteLine(newSource);
+Console.WriteLine("---------------------------------------");
+
+//List<String> buildConfig
